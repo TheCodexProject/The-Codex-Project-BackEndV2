56394118de13280b80b2c4600a662d9679df7830
BjarkeBP <Bjarkebp@live.dk>
2024-10-17 16:05:16 +0200

    Added ReadAllBoards endpoint
    This includes:
    - Endpoint
    - Command
    - Handler

---
+++ src/API/WebAPI/Endpoints/Board/ReadAllBoardEndpoint.cs
@@ -0,0 +1,62 @@
+﻿using application.appEntry.commands.board;
+using application.AppEntry.Commands.Board;
+using application.AppEntry.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using OperationResult;
+using WebAPI.Endpoints.Common;
+
+namespace webAPI.endpoints.board;
+
+public class ReadAllBoardEndpoint(ICommandDispatcher dispatcher) : ApiEndpoint.WithRequest<ReadAllBoardRequest>.WithResponse<ReadAllBoardResponse>
+{
+    [HttpPost("workspace")]
+    public override async Task<ActionResult<ReadAllBoardResponse>> HandleAsync(ReadAllBoardRequest request)
+    {
+        // Create the command
+        Result<ReadAllBoardsCommand> cmdResult = ReadAllBoardsCommand.Create();
+        if (cmdResult.IsFailure)
+        {
+            return BadRequest(cmdResult.Errors);
+        }
+
+        // Dispatch the command
+        Result result = await dispatcher.DispatchAsync(cmdResult.Value);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        // Convert Boards to BoardResponses
+        List<BoardResponse> boardResponses = cmdResult.Value.Boards.Select(board => new BoardResponse
+        {
+            Id = board.Id.ToString(),
+            Title = board.Title
+        }).ToList();
+
+        // Create the response object
+        ReadAllBoardResponse response = new ReadAllBoardResponse
+        {
+            boards = boardResponses,
+        };
+
+        return Ok(response);
+    }
+}
+
+public record ReadAllBoardRequest
+{
+    [FromRoute] public required string Id { get; set; }
+    [FromBody] public String? Title { get; init; }
+}
+
+public record ReadAllBoardResponse
+{
+    public List<BoardResponse>? boards { get; set; }
+}
+
+public record BoardResponse
+{
+    public string? Id { get; init; }
+    public String? Title { get; init; }
+}
